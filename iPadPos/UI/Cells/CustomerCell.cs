7e7ede70a649fe3ed7f2823aa10b336182249af8
James Clancey <james.clancey@gmail.com>
2014-05-07 20:14:00 -0800

    Added customer cell

---
+++ iPadPos/UI/Cells/CustomerCell.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MonoTouch.UIKit;
+
+namespace iPadPos
+{
+	public class CustomerCell : UITableViewCell
+	{
+		public const string Key = "CustomerCell";
+		public CustomerCell () : base (UITableViewCellStyle.Value2, Key)
+		{
+
+		}
+
+		Customer customer;
+		public Customer Customer {
+			get {
+				return customer;
+			}
+			set {
+				customer = value;
+				SetValues ();
+			}
+		}
+
+		void SetValues()
+		{
+			TextLabel.Text = Customer.ToString ();
+			DetailTextLabel.Text = Customer.Email;
+		}
+	}
+}
+
