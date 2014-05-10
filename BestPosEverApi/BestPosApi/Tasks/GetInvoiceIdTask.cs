e4f66ab17493066555a9789254f8c7ac7c39481e
Clancey <james.clancey@gmail.com>
2014-05-10 01:02:47 -0800

    Fixed GetInvoice Task

--- BestPosEverApi/BestPosApi/Tasks/GetInvoiceIdTask.cs
+++ BestPosEverApi/BestPosApi/Tasks/GetInvoiceIdTask.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Simpler;
+using WebApplication1.Helpers;
 using WebApplication1.Models;
 
 namespace WebApplication1.Tasks
@@ -13,12 +14,22 @@ namespace WebApplication1.Tasks
 		public string RegisterId { get; set; }
 		public override void Execute()
 		{
-			var sql =
-				string.Format("select {0}InvPrefix as Prefix, {0}InvSuffix as Suffix, {0}InvSize as Length from PosimRegisters where RegisterId = '{1}'",InvoiceStatus == InvoiceStatus.Posted ? "P" : "W",RegisterId );
-			Out = SharedDb.Get<SuffixPrefix>(sql).StringValue;
-			sql =
-				string.Format("update PosimRegisters set  {0}InvSuffix =  {0}InvSuffix + 1 where RegisterId = '{1}'", InvoiceStatus == InvoiceStatus.Posted ? "P" : "W", RegisterId);
-			SharedDb.Execute(sql);
+			bool exists = true;
+			while (exists)
+			{
+				var sql =
+					string.Format(
+						"select {0}InvPrefix as Prefix, {0}InvSuffix as Suffix, {0}InvSize as Length from PosimRegisters where RegisterId = '{1}'",
+						InvoiceStatus == InvoiceStatus.Posted ? "P" : "W", RegisterId);
+				Out = SharedDb.Get<SuffixPrefix>(sql).StringValue;
+				sql =
+					string.Format("update PosimRegisters set  {0}InvSuffix =  {0}InvSuffix + 1 where RegisterId = '{1}'",
+						InvoiceStatus == InvoiceStatus.Posted ? "P" : "W", RegisterId);
+				SharedDb.Execute(sql);
+				string existsString = string.Format("select count(InvoiceId) from {0}InvHeaders where InvoiceID = {1}",
+					InvoiceStatus == InvoiceStatus.Posted ? "P" : "W", Out.GetSqlCompatible());
+				exists = SharedDb.GetInt(existsString) > 0;
+			}
 		}
 	}
 }
